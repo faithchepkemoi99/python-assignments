081d3fda8ad9d2354a89496114a1303dd16013d0
faithchepkemoi99 <barakafay90@gmail.com>
2025-04-01 19:49:18 +0300

    wk3 submission

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+print("hello this is lesson 1")
+
+number = 34
+number2 = 23.8
+
+name = "Faith Chepkemoi B"
+dev = True
+
+print(name)
+print(dev)
+
+students = ["Janet", 34 , "Faith", "Baraka" , True, 3.4]
+print(students)
+print(students [4])
